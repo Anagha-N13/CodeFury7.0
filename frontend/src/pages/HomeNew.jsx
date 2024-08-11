54dfda25cd305faeb99f4d4d780dc2ec4c5cf5f1
Anagha-N13 <anagha2003.13@gmail.com>
2024-08-11 08:15:19 +0530

    changed home page styling

--- frontend/src/pages/HomeNew.jsx
+++ frontend/src/pages/HomeNew.jsx
@@ -1,24 +1,26 @@
 import Sidebar from '../components/sidebar/Sidebar';
-import React from 'react'
-import FeatureCards from '../components/cards/fcards'
+import React from 'react';
+import FeatureCards from '../components/cards/fcards';
 import Chatbot from '../components/chatbot/Chatbot';
-import '../CSS/home.css'
+import '../CSS/home.css';
 import MapComponent from '../components/map/map';
+
 function HomeNew() {
     return (
         <div className="home-container">
             <Sidebar />
-            <div className='main-content'>
-                <h1>DASHBOARD</h1>
-                <FeatureCards />
-                {/* <Chatbot/> */}
-
-            </div>
-            <div>
-                <MapComponent/>
+            <div className="main-container-new">
+                <div className="main-content">
+                    <h1>DASHBOARD</h1>
+                    <FeatureCards />
+                    {/* <Chatbot/> */}
+                </div>
+                <div className="map-container">
+                    <MapComponent />
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default HomeNew
+export default HomeNew;
